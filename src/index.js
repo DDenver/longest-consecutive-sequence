ca6b1426c78efaa5aed124fdf53d1260b36b75c0
Denis <den107620@gmail.com>
2018-03-03 19:04:12 +0300

    additi7

--- src/index.js
+++ src/index.js
@@ -1,3 +1,44 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+//правила сортировки
+  function fun(a, b) {
+    if (a > b) return 1;
+    if (a < b) return -1;
+  };
+
+  function unique(arr) {
+    var obj = {};
+  
+    for (var i = 0; i < arr.length; i++) {
+      var str = arr[i];
+      obj[str] = true; // запомнить строку в виде свойства объекта
+    }
+  
+    return Object.keys(obj); // или собрать ключи перебором для IE8-
+  }
+
+  array = unique(array.sort(fun));// сортируем массив и удаляем повторяющиеся элементы
+
+  let arrayLength = [];
+  let j = 0;
+  let p = false;
+
+  for (let i = 0; i < array.length; i++) { // определяем длину самой большой последовательности
+    if (array[i+1]-array[i] == 1){
+      arrayLength[j] = 1;
+      p = true;
+      j++;
+    } else if (p == true){
+      arrayLength[j] = 1;
+      p = false;
+      j = 0;
+    }
+  }
+
+  if (array.length == 0){
+    return 0;
+  } else if (arrayLength.length == 0){
+    return 1;
+  }
+
+  return arrayLength.length;
 }
